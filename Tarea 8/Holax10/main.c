d3bb4ab422c27bcc8a82addac03d314878b64e2c
Iván Salgado <sagmivan@gmail.com>
2017-10-14 21:53:36 -0500

    Add files via upload

---
+++ Tarea 8/Holax10/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Este programa imprime 10 Hola\n");  
+  int num=0;
+  for(num=1; num<=10; num=num+1)
+  {
+            
+           printf("Hola %d\n",num);
+           }
+  system("PAUSE");	
+  return 0;
+}
